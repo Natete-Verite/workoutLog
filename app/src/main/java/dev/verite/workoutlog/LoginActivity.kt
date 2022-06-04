0c1a8c02f26739cfea7cedf0ebdb5cc4b249df88
Natete-Verite <nateteverite@gmail.com>
2022-06-04 09:38:35 +0300

    Added my login activity

--- app/src/main/java/dev/verite/workoutlog/LoginActivity.kt
+++ app/src/main/java/dev/verite/workoutlog/LoginActivity.kt
@@ -2,10 +2,40 @@ package dev.verite.workoutlog
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
 
 class LoginActivity : AppCompatActivity() {
+    lateinit var btnLogin: Button
+    lateinit var tilEmail: TextInputLayout
+    lateinit var tilPassword: TextInputLayout
+    lateinit var etEmail: TextInputEditText
+    lateinit var etPassword: TextInputEditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        btnLogin = findViewById(R.id.btnLogin)
+        tilEmail = findViewById(R.id.tilEmail)
+        tilPassword = findViewById(R.id.tilPassword)
+        etEmail = findViewById(R.id.etEmail)
+        etPassword = findViewById(R.id.etPassword)
+
+        btnLogin.setOnClickListener {
+        validateLogin()
+        }
+    }
+
+    fun validateLogin() {
+        var email = etEmail.text.toString()
+        var password = etPassword.text.toString()
+        if (email.isBlank()) {
+            tilEmail.error = "Email is required"
+//            getString(string.email_required)
+        }
+        if (password.isBlank()) {
+            tilPassword.error = "Password is required"
+        }
     }
 }
