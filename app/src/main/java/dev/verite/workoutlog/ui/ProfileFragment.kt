8a499ce80a219bcdffcba3bc0795d72723904081
Natete-Verite <nateteverite@gmail.com>
2022-09-24 07:36:14 +0300

    Updated the Profile fragment

--- app/src/main/java/dev/verite/workoutlog/ui/ProfileFragment.kt
+++ app/src/main/java/dev/verite/workoutlog/ui/ProfileFragment.kt
@@ -1,6 +1,7 @@
 package dev.verite.workoutlog.ui
 
 import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -8,27 +9,34 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import dev.verite.workoutlog.R
+import dev.verite.workoutlog.databinding.FragmentProfileBinding
 import dev.verite.workoutlog.util.Constants
 
 class ProfileFragment : Fragment() {
-    lateinit var binding: Fragment
+    lateinit var binding: FragmentProfileBinding
+    lateinit var sharedPrefs:SharedPreferences
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-//        binding.tvLogout.setOnClickListener {
-//            sharedPrefs = getSharedPreferences(Constants.prefsFile, AppCompatActivity.MODE_PRIVATE)
-//            val editor = sharedPrefs.edit()
-//            editor.putString("ACCESS_TOKEN", "")
-//            editor.putString("USER_ID", "")
-//            editor.putString("PROFILE_ID", "")
-//            editor.apply()
-//
-//            startActivity(Intent(this,LoginActivity::class.java))
-//            finish()
-//        }
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+
+        binding=FragmentProfileBinding.inflate(inflater,container,false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.tvLogout.setOnClickListener {
+            sharedPrefs = this.requireActivity().getSharedPreferences(Constants.prefsFile, AppCompatActivity.MODE_PRIVATE)
+            val editor = sharedPrefs.edit()
+            editor.putString("ACCESS_TOKEN", "")
+            editor.putString("USER_ID", "")
+            editor.putString("PROFILE_ID", "")
+            editor.apply()
+
+            startActivity(Intent(this.context,LoginActivity::class.java))
+        }
     }
 }
